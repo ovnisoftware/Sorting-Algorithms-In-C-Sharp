fb8834364bd57fcfc17869f68d4355ee22847ddd
ovnisoftware <info@ovnisoftware.com>
2015-09-03 12:22:32 -0400

    Initial Commit

---
+++ Sorting-Algorithms-In-C-Sharp/Sorting-Algorithms-In-C-Sharp/Bubble_Sort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithms_In_C_Sharp
+{
+    //Sort an array of integers using Bubble Sort and no temp array
+    //(highest #'s bubbled to the right)
+    //Bubble Sort time complexity
+    //Best case: O(n) (If list is already sorted, it only needs to go through list one time, if bool flag added)
+    //Average case: O(N^2)
+    //Worst case: O(N^2) (Inversely sorted list (Ex. 4,3,2,1))
+    //https://www.youtube.com/watch?v=jAoBsroEow4
+    public static class Bubble_Sort
+    {
+        public static int[] BubbleSort(params int[] input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int k = 0; k < input.Length - 1 - i; k++)
+                {
+                    if (input[k] > input[k + 1])
+                    {
+                        int temp = input[k];
+                        input[k] = input[k + 1];
+                        input[k + 1] = temp;
+                    }
+                }
+            }
+            return input;
+        }
+        ////Call Program.cs to test
+        //int[] x = Bubble_Sort.BubbleSort(4, 3, 2, 1);
+        //foreach (int y in x)
+        //    Console.WriteLine(y);
+        ////Output should be:
+        ////1
+        ////2
+        ////3
+        ////4
+    }
+}
---
+++ Sorting-Algorithms-In-C-Sharp/Sorting-Algorithms-In-C-Sharp/Selection_Sort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithms_In_C_Sharp
+{
+    public static class Selection_Sort
+    {
+        //Selection Sort
+        //Write a program that traverses an array of integers, identifies the smallest integer
+        //and swaps it with the element at the far left in the zero index position.
+        //The program then will traverse the remainder of the array
+        //(excluding the first position which is already sorted) and repeat the process
+        //Selection Sort time complexity
+        //Best case: O(n^2)
+        //Average case: O(N^2)
+        //Worst case: O(N^2)
+        //https://www.youtube.com/watch?v=MZ-ZeQnUL1Q
+        public static int[] SelectionSort(params int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                int minPos = i;
+                for (int k = i + 1; k < input.Length; k++)
+                {
+                    if (input[minPos] > input[k])
+                        minPos = k;
+                }
+                //Smallest elements in the array are swapped with the elements to the farthest left
+                if (i != minPos)
+                {
+                    int temp = input[i];
+                    input[i] = input[minPos];
+                    input[minPos] = temp;
+                }
+            }
+            return input;
+        }
+        ////Call Program.cs to test
+        //int[] x = Selection_Sort.SelectionSort(4,3,2,1);
+        //foreach (int y in x)
+        //    Console.WriteLine(y);
+        ////Output should be:
+        ////1
+        ////2
+        ////3
+        ////4
+    }
+}
